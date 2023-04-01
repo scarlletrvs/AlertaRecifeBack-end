c9d0ba5b62357e8aeb9423c2763c820e7e62a032
sergiohdljr <sergio.tjf1@gmail.com>
2023-04-01 10:55:47 -0300

    Rotas/ criação de rotas get ocorrencias e ocorrenciasPorUsuario

--- src/route.ts
+++ src/route.ts
@@ -28,3 +28,38 @@ router.post('/ocorrencia', async (req, res) => {
     })
     res.json(resultado)
 })
+
+router.get('/ocorrencias', async (req, res) => {
+    const todasOcorrencias = await prisma.ocorrencia.findMany({
+        select: {
+            descricaoDaOcorrencia: true,
+            latitude: true,
+            longitude: true,
+            dataHora: true,
+            autorId: true,
+            autor: true,
+        }
+    })
+    res.json(todasOcorrencias)
+})
+
+router.get('/ocorrencias/:email', async (req, res) => {
+
+    const { email } = req.params
+
+    const ocorrenciasPorUsuario = await prisma.usuario.findUnique({
+        where: {
+            email,
+        },
+        select: {
+            id: true,
+            _count: true,
+            nome: true,
+            email: true,
+            fotoPerfil: true,
+            Ocorrencias: true
+        }
+    })
+
+    res.json(ocorrenciasPorUsuario)
+})
