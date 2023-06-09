b46c6b5f62368e4aa4167f20eba17387af861549
sergiohdljr <sergio.tjf1@gmail.com>
2023-06-09 20:58:43 -0300

    feat: endpoint delete ocorrencia

--- src/route.ts
+++ src/route.ts
@@ -1,72 +1,90 @@
 import { Router } from "express";
 import { prisma } from "./prisma";
 
-export const router = Router()
+export const router = Router();
 
 router.get("/", async (req, res) => {
-    res.json("dale mammi")
-})
+  res.json("dale mammi");
+});
 
-router.post('/ocorrencia', async (req, res) => {
+router.post("/ocorrencia", async (req, res) => {
+  const {
+    descricaoDaOcorrencia,
+    fotoOcorrencia,
+    enderecoOcorrencia,
+    nome,
+    email,
+    fotoPerfil,
+    tipoDaOcorrencia,
+  } = req.body;
 
-    const { descricaoDaOcorrencia, fotoOcorrencia, enderecoOcorrencia, nome, email, fotoPerfil, tipoDaOcorrencia } = req.body
-
-    const resultado = await prisma.ocorrencia.create({
-        data: {
-            fotoOcorrencia,
-            descricaoDaOcorrencia,
-            tipoDaOcorrencia,
-            enderecoOcorrencia,
-            autor: {
-                connectOrCreate: {
-                    where: {
-                        email,
-                    },
-                    create: {
-                        email,
-                        fotoPerfil,
-                        nome,
-                    }
-                }
-            }
-        }
-    })
-    res.json(resultado)
-})
-
-router.get('/ocorrencias', async (req, res) => {
-    const todasOcorrencias = await prisma.ocorrencia.findMany({
-        select: {
-            id: true,
-            fotoOcorrencia: true,
-            descricaoDaOcorrencia: true,
-            tipoDaOcorrencia: true,
-            enderecoOcorrencia: true,
-            dataHora: true,
-            autorId: true,
-            autor: true,
-        }
-    })
-    res.json(todasOcorrencias)
-})
-
-router.get('/ocorrencias/:email', async (req, res) => {
-
-    const { email } = req.params
-
-    const ocorrenciasPorUsuario = await prisma.usuario.findUnique({
-        where: {
+  const resultado = await prisma.ocorrencia.create({
+    data: {
+      fotoOcorrencia,
+      descricaoDaOcorrencia,
+      tipoDaOcorrencia,
+      enderecoOcorrencia,
+      autor: {
+        connectOrCreate: {
+          where: {
             email,
+          },
+          create: {
+            email,
+            fotoPerfil,
+            nome,
+          },
         },
-        select: {
-            id: true,
-            _count: true,
-            nome: true,
-            email: true,
-            fotoPerfil: true,
-            Ocorrencias: true
-        }
-    })
+      },
+    },
+  });
+  res.json(resultado);
+});
 
-    res.json(ocorrenciasPorUsuario)
-})
+router.get("/ocorrencias", async (req, res) => {
+  const todasOcorrencias = await prisma.ocorrencia.findMany({
+    select: {
+      id: true,
+      fotoOcorrencia: true,
+      descricaoDaOcorrencia: true,
+      tipoDaOcorrencia: true,
+      enderecoOcorrencia: true,
+      dataHora: true,
+      autorId: true,
+      autor: true,
+    },
+  });
+  res.json(todasOcorrencias);
+});
+
+router.get("/ocorrencias/:email", async (req, res) => {
+  const { email } = req.params;
+
+  const ocorrenciasPorUsuario = await prisma.usuario.findUnique({
+    where: {
+      email,
+    },
+    select: {
+      id: true,
+      _count: true,
+      nome: true,
+      email: true,
+      fotoPerfil: true,
+      Ocorrencias: true,
+    },
+  });
+
+  res.json(ocorrenciasPorUsuario);
+});
+
+router.delete("/delete/ocorrencia/:id", async (req, res) => {
+  const { id } = req.params;
+
+  const deletarOcorrencia = await prisma.ocorrencia.delete({
+    where: {
+      id,
+    },
+  });
+
+  res.json(deletarOcorrencia);
+});
